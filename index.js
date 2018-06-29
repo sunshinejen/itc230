34adb4b3eb4d74f1c906dcf8f3e5434b2330afb7
Jennifer Villacis <jennifer.villacis@seattlecentral.edu>
2018-06-29 01:23:46 +0000

    Updates home page text and modified server function

---
+++ index.js
@@ -0,0 +1,30 @@
+var http = require("http"); 
+var fs = require("fs");
+
+http.createServer(function(req,res) {
+  var path = req.url.toLowerCase();
+  
+  switch(path) {
+    case '/': 
+        fs.readFile('home.html', function (err, data) {
+            
+           
+           if (err) return console.error(err);
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data.toString());
+       
+        });
+        break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.end('Not found');
+      break;
+    }
+}).listen(process.env.PORT || 3000);
+ 
+ 
+ 

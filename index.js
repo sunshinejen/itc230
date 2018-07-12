bca9d5ac75cb68bd56f09ce8522763391e1029b9
Jennifer Villacis <jennifer.villacis@seattlecentral.edu>
2018-07-12 22:17:59 +0000

    Assignment2 commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var http = require("http"); 
 var fs = require("fs");
+var music = require('./lib/albums');
+var querystring = require('querystring');
 
 http.createServer(function(req,res) {
   var path = req.url.toLowerCase();
@@ -28,5 +30,36 @@ http.createServer(function(req,res) {
     }
 }).listen(process.env.PORT || 3000);
  
+ case '/get':
+     var result = music.get(music);
+     
+     if(result) {
+         res.writeHead(200, {'Content-Type: 'text/plain'});
+         res.end(result));
+     }else {
+             res.writeHead(200, {'Content-Type' : 'text/plain'});
+             res.end('Item not found');
+         };
+     // code
+     break;
+     
+     
+     case 'delete':
+         var deleteAlbum = music.splice(get(albumTitle));
+         var deleteResult= music.delete(deleteAlbum);
+         
+         if(deleteResult) {
+             res.writeHead(200, {'Content-Type' : 'text/plain'});
+             res.end('Album'+'deleteAlbum'+'has been removed');
+         } else {
+             res.writeHead(200, {'Content-Type' : 'text/plain'});
+             res.end('Album'+'deleteAlbum'+'not found');
+         }
+         break;
+     
  
  
+
+console.log(music.getAll());
+
+console.log(music.findAlbumTitle());

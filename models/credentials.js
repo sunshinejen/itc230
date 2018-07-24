b4e82f27a3fd7968b898617ffdf1a215e5d8084f
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2018-07-24 14:18:12 -0700

    delete important info

--- models/credentials.js
+++
@@ -1,6 +0,0 @@
-
-module.exports.connectionString = "mongodb://jennv:Summer90@ds239911.mlab.com:39911/jensprojects"; 
-
-
-// defines album model in JSON key/value pairs
-// values indicate the data type of each key

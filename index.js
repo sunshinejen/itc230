53d6edb9f09fd4a4585adda8a9852c8c0d9877b0
Jennifer Villacis <jennifer.villacis@seattlecentral.edu>
2018-08-03 04:45:56 +0000

    Fixes to Week 4 assignment, updates to routes

--- index.js
+++ index.js
@@ -3,49 +3,93 @@
 const express = require("express");
 const app = express();
 let handlebars = require("express-handlebars");
-var albumMethods = require("./models/albumMethods");
-var album =  require("./models/album");
+var Album =  require("./models/album");
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); 
 app.use(require("body-parser").urlencoded({extended:true}));
+app.use('/api', require('cors')()); // set Access-Control-Allow-Origin header for api route
 
 app.engine(".html", handlebars({extname: '.html'}));
 app.set("view engine", ".html");
-
-app.get('/', (req, res) => {
- res.type('text/html');
- res.sendFile(__dirname + '/public/home.html'); 
-});
 
 app.get('/about', (req, res) => {
  res.type('text/html');
 res.sendFile(__dirname + '/public/about.html');
 });
 
-//send content of home view
-app.get('/get', (req,res, next) => {
-   albumMethods.get( req.query.albumTitle.toLowerCase()).then((item) => {
-       res.render('details', {result:item});
-  }).catch((err) =>{
-    return next(err);
-  });
+
+app.get('/', (req,res,next) => {
+ Album.find((err, albums) => {
+  if (err) return next(err);
+  res.render('home', {albums:albums});
+ 
+ });
+ 
 });
 
+
+app.post('/get', (req,res, next) => {
+ console.log(req.body);
+ Album.findOne({albumtitle:req.query.albumtitle }, (err, albumtitle) => {
+  if (err) return next(err);
+  res.type('text/html');
+  res.render('details', {result:albumtitle});
+ });
+ 
+});
+  
+
 app.get('/delete', (req,res, next)=> {
- console.log(req.query.albumTitle);
- const dresult = req.query.albumTitle;
- console.log(dresult);
- albumMethods.delete(dresult).then((result) => {
-  album.count((err,count) => {
-   if (err) return next(err);
-  res.render('delete', {result: result.n, items:count, dresult:dresult});
-  });
-}).catch((err) => {
- return next(err);
+ console.log(req.query.albumtitle);
+ Album.remove({albumtitle:req.query.albumtitle }, (err, result) => {
+  if (err) return next(err);
+  let deleted = result.result.n !==0 //n will be 0 if no docs are deleted
+ Album.count((err, total) => {
+     res.type('text/html');
+     res.render('delete', {albumtitle:req.query.albumtitle, deleted: result.result.n !== 0, total: total});
+ });
+  
  });
 });
 
+//api's
+
+/*
+app.get('/api/v1/ablum/:albumtitle', (req, res, next) => {
+ let title = req.params.title;
+ console.log(title);
+ Album.findOne({title:title}, (err, result) => {
+  if (err || !result) return next(err);
+  res.json(result);
+ });
+});
+
+
+app.get('/api/v1/albums', (req, res, next) =>{
+ Album.find((err, results) => {
+  if (err || !results) return next(err);
+  res.json(results);
+ });
+});
+
+app.get('/api/v1/delete/:albumtitle', (req, res, next) =>{
+ Album.remove({"albumtitle":req.params.albumtitle}, (err, result) =>{
+      if (err) return next(err);
+      res.json({"deleted": result.result.n});
+ });
+});
+
+app.post('/api/v1/add', (req,res, next) => {
+
+    Album.update({ albumtitle:  req.body.albumtitle}, req.body, {upsert: true }, (err, result) => {
+        if (err) return next(err);
+        // nModified = 0 for new item, = 1+ for updated item 
+        res.json({updated: result.nModified});
+    });
+});
+
+*/
 app.use( (req,res) => {
  res.type('text/plain'); 
  res.status(404);
